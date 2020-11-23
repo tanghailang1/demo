86d5da194d77d184acaa4fc7d89aa0fca0d4fbb7
Yangchao Huang <jabez@efashionchina.com>
2020-11-23 15:39:55 +0800

    update application

--- src/main/java/com/efs/cloud/trackingservice/TrackingServiceApplication.java
+++ src/main/java/com/efs/cloud/trackingservice/TrackingServiceApplication.java
@@ -2,11 +2,13 @@ package com.efs.cloud.trackingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author jabez.huang
  */
 @SpringBootApplication
+@EnableAsync
 public class TrackingServiceApplication {
 
 	public static void main(String[] args) {
